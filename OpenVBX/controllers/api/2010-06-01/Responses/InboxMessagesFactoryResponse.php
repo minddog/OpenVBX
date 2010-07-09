56df29dabae6fe0f1638384bdcd649f0b29ee8cd
Adam Ballai <adam@blackacid.org>
2010-07-09 12:05:29 -0700

    Complete integration with annotations and replies

---
+++ OpenVBX/controllers/api/2010-06-01/Responses/InboxMessagesFactoryResponse.php
@@ -0,0 +1,105 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * "The contents of this file are subject to the Mozilla Public License
+ *  Version 1.1 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *  http://www.mozilla.org/MPL/
+ 
+ *  Software distributed under the License is distributed on an "AS IS"
+ *  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing rights and limitations
+ *  under the License.
+
+ *  The Original Code is OpenVBX, released June 15, 2010.
+
+ *  The Initial Developer of the Original Code is Twilio Inc.
+ *  Portions created by Twilio Inc. are Copyright (C) 2010.
+ *  All Rights Reserved.
+
+ * Contributor(s):
+ **/
+
+class InboxMessagesFactoryResponse extends RestResponse
+{
+	public function __construct($properties = array())
+	{
+		parent::__construct();
+		foreach($properties as $prop => $val)
+		{
+			$this->response->$prop = $val;
+		}
+	}
+
+	public function encode($format)
+	{
+		$ci = &get_instance();
+		$version = $ci->version;
+		
+		switch($format)
+		{
+			case 'json':
+				if(!is_object($this->response))
+					throw new Exception('Response data not an object');
+
+				$this->response->Version = $version;
+				
+				$messagesJSON = new stdClass();
+				$messagesJSON->Messages = array();
+				$messagesJSON->Total = $this->Total;
+				$messagesJSON->Offset = $this->Offset;
+				$messagesJSON->Max = $this->Max;
+
+				foreach($this->Messages as $message)
+				{
+					$messagesJSON->Messages[] =
+						array(
+							  'Sid' => $message->id,
+							  'From' => format_phone($message->caller),
+							  'To' => format_phone($message->called),
+							  'Body' => $message->content_text,
+							  'RecordingUrl' => preg_replace('/http:\/\//', 'https://', $message->content_url),
+							  'RecordingLength' => $message->content_url? format_player_time($message->size) : null,
+							  'Type' => $message->type,
+							  'TicketStatus' => $message->ticket_status,
+							  'Status' => $message->status,
+							  'Assigned' => $message->assigned_to,
+							  'Archived' => ($message->status == 'archived')? true : false,
+							  'Unread' => ($message->status == 'new')? true : false,
+							  'TimeReceived' => date('c', strtotime($message->created)),
+							  'LastUpdated' => date('c', strtotime($message->updated)),
+							  );
+				}
+				return json_encode($messagesJSON);
+				
+			case 'xml':
+				$xml = new SimpleXMLElement('<Response />');
+				$xml->addAttribute('version', $version);
+				$messagesXml = $xml->addChild('Messages');
+				foreach($this->Messages as $message)
+				{
+					/* Message Instance Properties */
+					$messageXml = $messagesXml->addChild('Message');
+					$messageXml->addChild('Sid', $message->id);
+					$messageXml->addChild('From', format_phone($message->caller));
+					$messageXml->addChild('To', format_phone($message->called));
+					$messageXml->addChild('Body', $message->content_text);
+					$messageXml->addChild('TimeReceived', $message->created);
+					$messageXml->addChild('LastUpdated', $message->updated);
+					$messageXml->addChild('RecordingUrl', $message->content_url);
+					$messageXml->addChild('RecordingLength', $message->content_url? format_player_time($message->size) : null);
+					$messageXml->addChild('Type', $message->type);
+					$messageXml->addChild('TicketStatus', $message->ticket_status);
+					$messageXml->addChild('Status', $message->status);
+					$messageXml->addChild('Archived', ($message->status == 'archived')? 'true' : 'false');
+					$messageXml->addChild('Unread', ($message->status == 'new')? 'true' : 'false');
+				}
+
+				/* Message Factory Properties */
+				$messagesXml->addAttribute('total', $this->Total);
+				$messagesXml->addAttribute('offset', $this->Offset);
+				$messagesXml->addAttribute('max', $this->Max);
+				
+				return $xml->asXML();
+		}
+	}
+}
---
+++ OpenVBX/controllers/api/2010-06-01/Responses/MessageInstanceResponse.php
@@ -0,0 +1,86 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * "The contents of this file are subject to the Mozilla Public License
+ *  Version 1.1 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *  http://www.mozilla.org/MPL/
+ 
+ *  Software distributed under the License is distributed on an "AS IS"
+ *  basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ *  License for the specific language governing rights and limitations
+ *  under the License.
+
+ *  The Original Code is OpenVBX, released June 15, 2010.
+
+ *  The Initial Developer of the Original Code is Twilio Inc.
+ *  Portions created by Twilio Inc. are Copyright (C) 2010.
+ *  All Rights Reserved.
+
+ * Contributor(s):
+ **/
+
+class MessageInstanceResponse extends RestResponse
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function encode($format)
+	{
+		$ci = &get_instance();
+		$version = $ci->version;
+		
+		$message = $this->Message;
+		switch($format)
+		{
+			case 'json':
+				$this->response->version = $version;
+				
+				$messageJSON = array(
+									 'Sid' => $this->Sid,
+									 'From' => format_phone($message->caller),
+									 'To' => format_phone($message->called),
+									 'Body' => $message->content_text,
+									 'RecordingUrl' => preg_replace('/http:\/\//', 'https://', $message->content_url),
+									 'RecordingLength' => $message->content_url? format_player_time($message->size) : null,
+									 'Type' => $message->type,
+									 'TicketStatus' => $message->ticket_status,
+									 'Status' => $message->status,
+									 'Assigned' => $message->assigned_to,
+									 'Archived' => ($message->status == 'archived')? true : false,
+									 'Unread' => ($message->status == 'new')? true : false,
+									 'TimeReceived' => date('c', strtotime($message->created)),
+									 'LastUpdated' => date('c', strtotime($message->updated)),
+									 );
+
+				return json_encode($messageJSON);
+			case 'xml':
+				$xml = new SimpleXMLElement('<Response />');
+				$xml->addAttribute('version', $version);
+				
+				/* Message Instance Properties */
+				$messageXml = $xml->addChild('Message');
+				$messageXml->addChild('Sid', $message->id);
+				$messageXml->addChild('From', format_phone($message->caller));
+				$messageXml->addChild('To', format_phone($message->called));
+				$messageXml->addChild('Body', $message->content_text);
+				$messageXml->addChild('TimeReceived', $message->created);
+				$messageXml->addChild('LastUpdated', $message->updated);
+				$messageXml->addChild('RecordingUrl', $message->content_url);
+				$messageXml->addChild('RecordingLength', $message->content_url? format_player_time($message->size) : null);
+				$messageXml->addChild('Type', $message->type);
+				$messageXml->addChild('TicketStatus', $message->ticket_status);
+				$messageXml->addChild('Status', $message->status);
+				$messageXml->addChild('Archived', ($message->status == 'archived')? 'true' : 'false');
+				$messageXml->addChild('Unread', ($message->status == 'new')? 'true' : 'false');
+				
+				return $xml->asXML();
+				
+		}
+		
+		throw new RestResourceException("Format not supported: $format");
+	}
+
+}
+
